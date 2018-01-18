6f83dfc5f92c5a582311501375ecbe8650fcb7b3
Lawrence Neal <nealla@lwneal.com>
2018-01-17 18:13:33 -0800

    Fix order of coordinates
    Add HumanBot

--- environments.py
+++ environments.py
@@ -3,11 +3,11 @@ import numpy as np
 
 DIRTY, CLEAN, WALL = 0, 1, 2
 #character_map = ['D ', '  ', 'W ']
-character_map = ['\xf0\x9f\x92\xa9 ', ' ', 'W ']
-robot_char_map = ['\xe2\x87\xa7', '\xe2\x87\xa8', '\xe2\x87\xa9', '\xe2\x87\xa6']
+character_map = ['\xf0\x9f\x92\xa9 ', '\xe2\xac\x9b ', '\xe2\xac\x9c ']
+robot_char_map = ['\xe2\x87\xa7 ', '\xe2\x87\xa8 ', '\xe2\x87\xa9 ', '\xe2\x87\xa6 ']
 
 NORTH, EAST, SOUTH, WEST = 0, 1, 2, 3
-velocity_map = [(-1, 0), (1, 0), (1, 0), (-1, 0)]
+velocity_map = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 ACTIONS = ['left', 'right', 'forward', 'suck', 'off']
 
@@ -21,7 +21,7 @@ class VacuumWorld():
     height = 10
     def __init__(self):
         # Each cell can be dirty, clean, or wall
-        self.grid = np.random.randint(3, size=(self.width, self.height))
+        self.grid = np.random.randint(3, size=(self.height, self.width))
         # Start the the bottom-left corner facing upwards
         self.x, self.y = 0, self.height - 1
         self.direction = NORTH
@@ -42,16 +42,16 @@ class VacuumWorld():
         if action == 'off':
             pass
         elif action == 'suck':
-            self.grid[self.x, self.y] = CLEAN
+            self.grid[self.y, self.x] = CLEAN
         elif action == 'right':
             self.direction = (self.direction + 1) % 4
         elif action == 'left':
             self.direction = (self.direction - 1) % 4
         elif action == 'forward':
             dx, dy = velocity_map[self.direction]
-            new_x = np.clip(self.x + dx, 0, self.width)
-            new_y = np.clip(self.y + dy, 0, self.height)
-            if self.grid[new_x, new_y] != WALL:
+            new_x = np.clip(self.x + dx, 0, self.width - 1)
+            new_y = np.clip(self.y + dy, 0, self.height - 1)
+            if self.grid[new_y, new_x] != WALL:
                 self.x, self.y = new_x, new_y
         else:
             raise ValueError("Unknown action {}".format(action))
@@ -60,10 +60,11 @@ class VacuumWorld():
     def print_state(self):
         for i, line in enumerate(self.grid):
             for j, square in enumerate(line):
-                if (i, j) == (self.x, self.y):
+                if (j, i) == (self.x, self.y):
                     sys.stdout.write(robot_char_map[self.direction])
                 else:
                     sys.stdout.write(character_map[square])
             sys.stdout.write('\n')
+        print(self.grid)
         print("Direction: {}".format(self.direction))
         print('Position: ({}, {})'.format(self.x, self.y))
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ agent = agents.HumanBot()
 env = environments.VacuumWorld()
 
 for i in range(MAX_ITERS):
+    time.sleep(.5)
+    env.print_state()
     percepts = env.get_percepts()
     action = agent.act(percepts)
     env.update(action)
-    env.print_state()
-    time.sleep(.5)
