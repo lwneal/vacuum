a63735e0f07760e9fd78cc0d3e5ddf4a9cd8b096
Lawrence Neal <nealla@lwneal.com>
2018-01-19 13:19:41 -0800

    Add random runs

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+echo "Running standard agents..."
 for agent in SimpleReflexAgent  RandomizedReflexAgent ModelBasedAgent; do
     for world in EmptyRoom FourRooms; do
         python main.py $agent $world.txt 1000 --graph > ${agent}_${world}.csv
         python plot.py ${agent}_${world}.csv
         echo "Output ${agent}_${world}.png"
     done
+done
+
+agent=RandomizedReflexAgent
+ITERS=50
+echo "Running randomized agent $ITERS times"
+for world in EmptyRoom FourRooms; do
+    for i in `seq $ITERS`; do
+        python main.py $agent $world.txt 50 --graph > ${agent}_${world}_$i.csv
+    done
+    python avg_csv.py ${agent}_${world}_*.csv
+    python plot.py averaged_${agent}_${world}.csv
 done
